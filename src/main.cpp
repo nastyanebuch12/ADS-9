c5f53f418df3731185e351a4b6cbb42484362680
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-06-06 20:58:31 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 NNTU-CS
-#include <chrono>
+#include <std::chrono>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <vector>
 #include "tree.h"
-using namespace std;
 
 int factorial(int n) {
   int res = 1;
@@ -14,24 +13,24 @@ int factorial(int n) {
 }
 void printVector(const vector<char>& vector) {
   for (char ch : vector)
-    cout << ch;
-  cout << endl;
+    std::cout << ch;
+  std::cout << endl;
 }
 
 int main() {
   srand(time(0));
   vector<char> set = { '1', '2', '3', '4'};
   PMTree tree(set);
-  cout << "getAllPerms:" << endl;
+  std::cout << "getAllPerms:" << endl;
   auto perms = getAllPerms(tree);
   for (const auto& p : perms) printVector(p);
-  cout << endl;
-  cout << "getPerm1 with number 3: ";
+  std::cout << endl;
+  std::cout << "getPerm1 with number 3: ";
   printVector(getPerm1(tree, 3));
-  cout << "getPerm2 with number 5: ";
+  std::cout << "getPerm2 with number 5: ";
   printVector(getPerm2(tree, 5));
-  cout << endl;
-  cout << "n\t" << "getAllPerms(ms) "
+  std::cout << endl;
+  std::cout << "n\t" << "getAllPerms(ms) "
        << "getPerm1(ms) " << "getPerm2(ms)" << endl;
   for (int n = 1; n <= 10; ++n) {
     vector<char> set1;
@@ -39,24 +38,24 @@ int main() {
     PMTree tree(set1);
     int total = factorial(n);
     int num = rand_r() % total + 1;
-    auto time1_start = chrono::high_resolution_clock::now();
+    auto time1_start = std::chrono::high_resolution_clock::now();
     getAllPerms(tree);
-    auto time1_end = chrono::high_resolution_clock::now();
+    auto time1_end = std::chrono::high_resolution_clock::now();
     double time1 =
-    chrono::duration<double, std::milli>(time1_end - time1_start).count();
+    std::chrono::duration<double, std::milli>(time1_end - time1_start).count();
 
-    auto time2_start = chrono::high_resolution_clock::now();
+    auto time2_start = std::chrono::high_resolution_clock::now();
     getPerm1(tree, num);
-    auto time2_end = chrono::high_resolution_clock::now();
+    auto time2_end = std::chrono::high_resolution_clock::now();
     double time2 =
-    chrono::duration<double, std::milli>(time2_end - time2_start).count();
+    std::chrono::duration<double, std::milli>(time2_end - time2_start).count();
 
-    auto time3_start = chrono::high_resolution_clock::now();
+    auto time3_start = std::chrono::high_resolution_clock::now();
     getPerm2(tree, num);
-    auto time3_end = chrono::high_resolution_clock::now();
+    auto time3_end = std::chrono::high_resolution_clock::now();
     double time3 =
-    chrono::duration<double, std::milli>(time3_end - time3_start).count();
-    cout << n << "\t" << time1 << "\t\t" << time2 << "\t\t" << time3 << endl;
+    std::chrono::duration<double, std::milli>(time3_end - time3_start).count();
+    std::cout << n << "\t" << time1 << "\t\t" << time2 << "\t\t" << time3 << endl;
   }
   return 0;
 }
