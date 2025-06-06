6d7c636ecb621737700326bab691287825d7a7e0
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-06-06 21:10:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,33 @@ int factorial(int n) {
   for (int i = 2; i <= n; ++i) res *= i;
   return res;
 }
-void printVector(const vector<char>& vector) {
-  for (char ch : vector)
+void printVector(const std::vector<char>& v) {
+  for (char ch : v)
     std::cout << ch;
-  std::cout << endl;
+  std::cout << "\n";
 }
 
 int main() {
-  srand(time(0));
-  vector<char> set = { '1', '2', '3', '4'};
+  std::srand(time(0));
+  std::vector<char> set = { '1', '2', '3', '4'};
   PMTree tree(set);
-  std::cout << "getAllPerms:" << endl;
+  std::cout << "getAllPerms:" << "\n";
   auto perms = getAllPerms(tree);
   for (const auto& p : perms) printVector(p);
-  std::cout << endl;
+  std::cout << "\n";
   std::cout << "getPerm1 with number 3: ";
   printVector(getPerm1(tree, 3));
   std::cout << "getPerm2 with number 5: ";
   printVector(getPerm2(tree, 5));
-  std::cout << endl;
+  std::cout << "\n";
   std::cout << "n\t" << "getAllPerms(ms) "
-       << "getPerm1(ms) " << "getPerm2(ms)" << endl;
+       << "getPerm1(ms) " << "getPerm2(ms)" << "\n";
   for (int n = 1; n <= 10; ++n) {
-    vector<char> set1;
+    std::vector<char> set1;
     for (int i = 0; i < n; ++i) set1.push_back('a' + i);
     PMTree tree(set1);
     int total = factorial(n);
-    int num = rand_r() % total + 1;
+    int num = std::rand() % total + 1;
     auto time1_start = std::chrono::high_resolution_clock::now();
     getAllPerms(tree);
     auto time1_end = std::chrono::high_resolution_clock::now();
@@ -56,7 +56,7 @@ int main() {
     double time3 =
     std::chrono::duration<double, std::milli>(time3_end - time3_start).count();
     std::cout << n << "\t" << time1 << "\t\t" << time2 << "\t\t" << time3
-              << endl;
+              << "\n";
   }
   return 0;
 }
