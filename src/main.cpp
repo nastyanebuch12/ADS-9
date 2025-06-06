b630d1b90c2b5e803d92f292a41b7894ffcc96f3
nastyanebuch12 <145349015+nastyanebuch12@users.noreply.github.com>
2025-06-06 20:45:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,58 @@
 // Copyright 2022 NNTU-CS
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <vector>
 #include "tree.h"
+using namespace std;
+
+int factorial(int n) {
+  int res = 1;
+  for (int i = 2; i <= n; ++i) res *= i;
+  return res;
+}
+void printVector(const vector<char>& vector) {
+  for (char ch : vector)
+    cout << ch;
+  cout << endl;
+}
 
 int main() {
+  srand(time(0));
+  vector<char> set = { '1', '2', '3', '4'};
+  PMTree tree(set);
+  cout << "getAllPerms:" << endl;
+  auto perms = getAllPerms(tree);
+  for (const auto& p : perms) printVector(p);
+  cout << endl;
+  cout << "getPerm1 with number 3: ";
+  printVector(getPerm1(tree, 3));
+  cout << "getPerm2 with number 5: ";
+  printVector(getPerm2(tree, 5));
+  cout << endl;
+  cout << "n\t" << "getAllPerms(ms) " << "getPerm1(ms) " << "getPerm2(ms)" << endl;
+  for (int n = 1; n <= 10; ++n) {
+    vector<char> set1;
+    for (int i = 0; i < n; ++i) set1.push_back('a' + i);
+    PMTree tree(set1);
+    int total = factorial(n);
+    int num = rand() % total + 1;
+    auto time1_start = chrono::high_resolution_clock::now();
+    getAllPerms(tree);
+    auto time1_end = chrono::high_resolution_clock::now();
+    double time1 = chrono::duration<double, std::milli>(time1_end - time1_start).count();
+
+    auto time2_start = chrono::high_resolution_clock::now();
+    getPerm1(tree, num);
+    auto time2_end = chrono::high_resolution_clock::now();
+    double time2 = chrono::duration<double, std::milli>(time2_end - time2_start).count();
+
+    auto time3_start = chrono::high_resolution_clock::now();
+    getPerm2(tree, num);
+    auto time3_end = chrono::high_resolution_clock::now();
+    double time3 = chrono::duration<double, std::milli>(time3_end - time3_start).count();
+    cout << n << "\t" << time1 << "\t\t" << time2 << "\t\t" << time3 << endl;
+  }
   return 0;
 }
